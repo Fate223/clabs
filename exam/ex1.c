dd0faf6af3d813efb64c31a239cca98f274fae6f
FaTe223 <617673087@qq.com>
2019-11-15 00:21:35 -0800

    exam

---
+++ exam/ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define MAXLINE 50
+main()
+{
+    int x,b,c;
+    b=c=0;
+    x = getchar();
+    while (x="a"&&"A"){
+        b++;
+    }
+    while (x=0){
+        c++;
+    }
+    printf("the number of a is :%u\n",b);
+    printf("the number of 0 is :%u\n",c);
+}
